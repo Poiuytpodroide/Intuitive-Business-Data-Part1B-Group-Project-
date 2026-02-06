3b7c31e300d72a838c804ab44f43d807f1ffa78e
AragornOfKebroyd <benjamin.tinsley54@gmail.com>
2026-02-06 22:04:00 +0000

    Added database and test Flask routes

---
+++ backend/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+import database
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def index():
+    return "Flask API"
+
+@app.route("/sqlTest")
+def sql_test(): 
+    return database.get_services()
+
+@app.route("/api/getVPL=<vplID>")
+def home_page(vplID):
+    return vplID # get vpl file
+
+@app.teardown_appcontext
+def on_close(exception):
+    database.close_connection(exception)
