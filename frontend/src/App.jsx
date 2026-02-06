7c935f0990d08c811c833ea4300e95318d101461
ParthB253 <bparth1202@gmail.com>
2026-02-06 13:56:07 +0000

    added gitignore, set up basic frontend.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import Table from "./components/Table";
+
+function App() {
+    return (
+        <main className="w-screen h-screen">
+            <Table />
+        </main>
+    );
+}
+
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-function Hello() {
-  return (
-    <h1>Hello World!</h1>
-  );
-}
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <Hello />
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
