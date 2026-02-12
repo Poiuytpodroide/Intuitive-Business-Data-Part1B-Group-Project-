25a9547a8bf3eb7b93ef17a509a2897a718e860d
AragornOfKebroyd <benjamin.tinsley54@gmail.com>
2026-02-12 23:33:56 +0000

    changed database schema and added aggregation

--- backend/data/setupDB.sql
+++ backend/data/setupDB.sql
@@ -1,51 +1,3 @@
-UPDATE hundred_k
-SET
-AvailabilityZone = NULLIF(AvailabilityZone, 'NULL'),
-BilledCost = NULLIF(BilledCost, 'NULL'),
-BillingAccountId = NULLIF(BillingAccountId, 'NULL'),
-BillingAccountName = NULLIF(BillingAccountName, 'NULL'),
-BillingCurrency = NULLIF(BillingCurrency, 'NULL'),
-BillingPeriodEnd = NULLIF(BillingPeriodEnd, 'NULL'),
-BillingPeriodStart = NULLIF(BillingPeriodStart, 'NULL'),
-ChargeCategory = NULLIF(ChargeCategory, 'NULL'),
-ChargeClass = NULLIF(ChargeClass, 'NULL'),
-ChargeDescription = NULLIF(ChargeDescription, 'NULL'),
-ChargeFrequency = NULLIF(ChargeFrequency, 'NULL'),
-ChargePeriodEnd = NULLIF(ChargePeriodEnd, 'NULL'),
-ChargePeriodStart = NULLIF(ChargePeriodStart, 'NULL'),
-CommitmentDiscountCategory = NULLIF(CommitmentDiscountCategory, 'NULL'),
-CommitmentDiscountId = NULLIF(CommitmentDiscountId, 'NULL'),
-CommitmentDiscountName = NULLIF(CommitmentDiscountName, 'NULL'),
-CommitmentDiscountStatus = NULLIF(CommitmentDiscountStatus, 'NULL'),
-CommitmentDiscountType = NULLIF(CommitmentDiscountType, 'NULL'),
-ConsumedQuantity = NULLIF(ConsumedQuantity, 'NULL'),
-ConsumedUnit = NULLIF(ConsumedUnit, 'NULL'),
-ContractedCost = NULLIF(ContractedCost, 'NULL'),
-ContractedUnitPrice = NULLIF(ContractedUnitPrice, 'NULL'),
-EffectiveCost = NULLIF(EffectiveCost, 'NULL'),
-InvoiceIssuerName = NULLIF(InvoiceIssuerName, 'NULL'),
-ListCost = NULLIF(ListCost, 'NULL'),
-ListUnitPrice = NULLIF(ListUnitPrice, 'NULL'),
-PricingCategory = NULLIF(PricingCategory, 'NULL'),
-PricingQuantity = NULLIF(PricingQuantity, 'NULL'),
-PricingUnit = NULLIF(PricingUnit, 'NULL'),
-ProviderName = NULLIF(ProviderName, 'NULL'),
-PublisherName = NULLIF(PublisherName, 'NULL'),
-RegionId = NULLIF(RegionId, 'NULL'),
-RegionName = NULLIF(RegionName, 'NULL'),
-ResourceId = NULLIF(ResourceId, 'NULL'),
-ResourceName = NULLIF(ResourceName, 'NULL'),
-ResourceType = NULLIF(ResourceType, 'NULL'),
-ServiceCategory = NULLIF(ServiceCategory, 'NULL'),
-Id = NULLIF(Id, 'NULL'),
-ServiceName = NULLIF(ServiceName, 'NULL'),
-SkuId = NULLIF(SkuId, 'NULL'),
-SkuPriceId = NULLIF(SkuPriceId, 'NULL'),
-SubAccountId = NULLIF(SubAccountId, 'NULL'),
-SubAccountName = NULLIF(SubAccountName, 'NULL'),
-Tags = NULLIF(Tags, 'NULL');
-
-
 UPDATE focus_raw
 SET
 AvailabilityZone = NULLIF(AvailabilityZone, 'NULL'),
@@ -101,68 +53,170 @@ SELECT *,
     json_extract(Tags,"$.application") AS application
 FROM focus_raw;
 
-
-
-CREATE TABLE cost_entity (
-    cost_entity_id TEXT PRIMARY KEY,
-    provider TEXT,
-    entity_type TEXT,
-    display_name TEXT
+CREATE TABLE billing_account (
+    billing_account_id TEXT PRIMARY KEY,
+    billing_account_name TEXT
 );
 
-INSERT OR IGNORE INTO cost_entity
+INSERT OR IGNORE INTO billing_account
 SELECT DISTINCT
-  CASE
-    WHEN ProviderName != "Google Cloud" THEN ProviderName || '::' || SubAccountName
-    WHEN business_unit IS NOT NULL THEN 'GCP::' || business_unit
-    WHEN application IS NOT NULL THEN 'GCP::' || application
-    ELSE 'GCP::Unassigned'
-  END AS cost_entity_id,
-
-  ProviderName AS provider,
-
-  CASE
-    WHEN ProviderName != "Google Cloud" THEN 'subAccount'
-    WHEN business_unit IS NOT NULL THEN 'business_unit'   
-    WHEN application IS NOT NULL THEN 'application'
-    ELSE 'unattributed'
-  END AS entity_type,
-
-  CASE
-    WHEN ProviderName != "Google Cloud" THEN SubAccountName
-    WHEN business_unit IS NOT NULL THEN business_unit
-    WHEN application IS NOT NULL THEN application
-    ELSE 'Unattributed (GCP)'
-  END AS display_name
-FROM focus_with_tags;
-
-CREATE TABLE service (
-    service_id TEXT PRIMARY KEY,
-    provider TEXT
-);
-
-INSERT OR IGNORE INTO service
-SELECT DISTINCT
-ServiceName AS service_id,
-ProviderName as provider
+    BillingAccountId AS billing_account_id,
+    BillingAccountName AS billing_account_name
 FROM focus_raw;
 
--- need to check what other rows are relevant, for example is ChargeCategory relevant?
+CREATE TABLE sub_account (
+    sub_account_id TEXT PRIMARY KEY,
+    sub_account_name TEXT
+);
 
+INSERT OR IGNORE INTO sub_account
+SELECT DISTINCT
+    SubAccountId AS sub_account_id,
+    SubAccountName AS sub_account_name
+FROM focus_raw WHERE SubAccountId IS NOT NULL;
+
+DROP TABLE focus_usage_cost;
 CREATE TABLE focus_usage_cost (
-    id INT PRIMARY KEY,
-    time DATETIME,
-    cost_entity_id TEXT,
-    service_id TEXT,
-    serviceCategory TEXT,
-    region TEXT,
+    provider_name TEXT NOT NULL,
+    billing_account_id TEXT NOT NULL,
+    sub_account_id TEXT,
+    service_category TEXT,
+    service_name TEXT,
+    region_id TEXT,
+    resource_id TEXT,
+    application TEXT,
+    business_unit TEXT,
+    charge_start_time DATETIME NOT NULL,
+    charge_end_time DATETIME NOT NULL,
+    billed_cost REAL NOT NULL,
+    effective_cost REAL NOT NULL,
+    currency TEXT,
     usage_quantity REAL,
     usage_unit TEXT,
-    cost REAL,
-    currency TEXT,
     description TEXT,
-    FOREIGN KEY (cost_entity_id) REFERENCES cost_entity(cost_entity_id),
-    FOREIGN KEY (service_id) REFERENCES service(service_id)
-)
+    
+    FOREIGN KEY (billing_account_id) REFERENCES billing_account(billing_account_id),
+    FOREIGN KEY (sub_account_id) REFERENCES sub_account(sub_account_id)
+);
 
--- populate the cut down version of the table
+INSERT OR IGNORE INTO focus_usage_cost
+SELECT
+    ProviderName AS provider_name,
+    BillingAccountId AS billing_account_id,
+    SubAccountId AS sub_account_id,
+    ServiceCategory AS service_category,
+    ServiceName AS service_name,
+    RegionId AS region_id,
+    ResourceId AS resource_id,
+    application,
+    business_unit,
+    DATETIME(ChargePeriodStart) AS charge_start_time,
+    DATETIME(ChargePeriodEnd) AS charge_end_time,
+    BilledCost AS billed_cost,
+    EffectiveCost AS effective_cost,
+    BillingCurrency AS currency,
+    ConsumedQuantity AS usage_quantity,
+    ConsumedUnit AS usage_unit,
+    ChargeDescription as description    
+FROM focus_with_tags;
+
+DROP TABLE focus_usage_cost_hourly;
+CREATE TABLE focus_usage_cost_hourly AS
+SELECT
+    provider_name,
+    billing_account_id,
+    sub_account_id,
+    service_category,
+    service_name,
+    region_id,
+    resource_id,
+    application,
+    business_unit,
+    charge_start_time AS usage_hour,
+    SUM(billed_cost) AS billed_cost,
+    SUM(CASE WHEN effective_cost < 0 THEN effective_cost ELSE 0 END) AS total_credits,
+    SUM(CASE WHEN effective_cost >= 0 THEN effective_cost ELSE 0 END) AS total_usage_cost,
+    SUM(effective_cost) AS net_cost,
+    SUM(usage_quantity) AS usage_quantity,
+    usage_unit
+FROM focus_usage_cost
+GROUP BY
+    provider_name, billing_account_id, sub_account_id,
+    service_category, service_name, region_id, resource_id,
+    application, business_unit, usage_hour, usage_unit;
+
+
+DROP TABLE focus_usage_cost_daily;
+CREATE TABLE focus_usage_cost_daily AS
+SELECT
+    provider_name,
+    billing_account_id,
+    sub_account_id,
+    service_category,
+    service_name,
+    region_id,
+    resource_id,
+    application,
+    business_unit,
+    DATE(charge_start_time) AS usage_date,
+    SUM(billed_cost) AS billed_cost,
+    SUM(CASE WHEN effective_cost < 0 THEN effective_cost ELSE 0 END) AS total_credits,
+    SUM(CASE WHEN effective_cost >= 0 THEN effective_cost ELSE 0 END) AS total_usage_cost,
+    SUM(effective_cost) AS net_cost,
+    SUM(usage_quantity) AS usage_quantity,
+    usage_unit
+FROM focus_usage_cost
+GROUP BY
+    provider_name, billing_account_id, sub_account_id,
+    service_category, service_name, region_id, resource_id,
+    application, business_unit, usage_date, usage_unit;
+    
+DROP TABLE focus_usage_cost_weekly;
+CREATE TABLE focus_usage_cost_weekly AS
+SELECT
+    provider_name,
+    billing_account_id,
+    sub_account_id,
+    service_category,
+    service_name,
+    region_id,
+    resource_id,
+    application,
+    business_unit,
+    strftime('%Y-%W', charge_start_time) AS usage_week,
+    SUM(billed_cost) AS billed_cost,
+    SUM(CASE WHEN effective_cost < 0 THEN effective_cost ELSE 0 END) AS total_credits,
+    SUM(CASE WHEN effective_cost >= 0 THEN effective_cost ELSE 0 END) AS total_usage_cost,
+    SUM(effective_cost) AS net_cost,
+    SUM(usage_quantity) AS usage_quantity,
+    usage_unit
+FROM focus_usage_cost
+GROUP BY
+    provider_name, billing_account_id, sub_account_id,
+    service_category, service_name, region_id, resource_id,
+    application, business_unit, usage_week, usage_unit;
+    
+DROP TABLE focus_usage_cost_monthly;
+CREATE TABLE focus_usage_cost_monthly AS
+SELECT
+    provider_name,
+    billing_account_id,
+    sub_account_id,
+    service_category,
+    service_name,
+    region_id,
+    resource_id,
+    application,
+    business_unit,
+    strftime('%Y-%m', charge_start_time) AS usage_month,
+    SUM(billed_cost) AS billed_cost,
+    SUM(CASE WHEN effective_cost < 0 THEN effective_cost ELSE 0 END) AS total_credits,
+    SUM(CASE WHEN effective_cost >= 0 THEN effective_cost ELSE 0 END) AS total_usage_cost,
+    SUM(effective_cost) AS net_cost,
+    SUM(usage_quantity) AS usage_quantity,
+    usage_unit
+FROM focus_usage_cost
+GROUP BY
+    provider_name, billing_account_id, sub_account_id,
+    service_category, service_name, region_id, resource_id,
+    application, business_unit, usage_month, usage_unit;
